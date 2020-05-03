9d4a6072a8f2b9671fbd23992d9c1bd4df124f49
Harley McDowell <hmcdowell95@gmail.com>
2020-05-03 17:12:30 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -16,7 +16,7 @@ class PetsController < ApplicationController
     #binding.pry
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
-      puts "\nparams is: #{params}"
+      #puts "\nparams is: #{params}"
     end
     redirect to "pets/#{@pet.id}"
   end
